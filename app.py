51822e6454f69d043d0bef44ce1e1cb3d7094575
fahmi idriss <idrissfahmi631@gmail,com>
2024-08-14 18:23:42 +0200

    new pages added

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask,render_template, request , flash ,url_for
+from flask import Flask,render_template, request , flash ,url_for ,session
 
 app=Flask(__name__)
-app.secret_key="MyKey"
+
+app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
 
 @app.route('/')
 def home():
-    flash("Bienvenue ")
+    
     return render_template('index.html' )
 
 
-@app.route('/se connecter')
-def seConnecter():
+@app.route('/login')
+def login():
+     
 
-    return render_template('se Connecter.html' )
+     data=request.form
+
+     return render_template('login.html' )
 
 
 @app.route('/propos')
@@ -32,12 +36,14 @@ def formatiion():
 
 @app.route('/signup',methods=['GET','POST'])
 def signup(): 
-        flash('Welcome to the signup Page,' , category='error')
-        if request.form =='POST':
-                email = request.form('email')
-                firstName = request.form ('firstName')
-                password1 = request.form('password1')
-                password2 = request.form ('password2')  
+
+  
+    if request.form =='POST':
+            
+                email = request.form.get('email')
+                firstName = request.form.get ('firstName')
+                password1 = request.form .get('password1')
+                password2 = request.form .get('password2')  
                 if len(email) < 4 :  
                     flash("Votre Email Et Deja En Cours")
                    
@@ -50,18 +56,47 @@ def signup():
                     flash("password  must be greater tha 7 characters.", category='error')
                 else:
                     flash ('welcome ', category='error')  
-        return render_template("signup.html")
+    return render_template("signup.html")
        
     
 @app.route('/book1')
 def book1():
+    
     return render_template('book1.html'  )
 
 
 @app.route('/book1/<int:lesson_id>')
 def lesson(lesson_id):
     
-    return f'lesson {lesson_id}'
+    return f'lesson  {lesson_id }'
+  
+@app.route('/book2')
+def book2():
+    
+    return render_template('book2.html'  )
+
+
+@app.route('/book2/<int:lessons_id>')
+def lessons(lessons_id):
+    
+    return f'lesson  {lessons_id }'
+ 
+
+
+ 
+@app.route('/book3')
+def book3():
+    
+    return render_template('book3.html'  )
+
+
+@app.route('/book3/<int:lessons1_id>')
+def lessons1(lessons_id):
+    
+    return f'lesson  {lessons_id }'
+
+
+
 
 if __name__ =='__main__':
     app.run(debug= True )
